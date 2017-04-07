44f4d97e52db1f142238e7085d31611210d9d924
Grégory <claudejurado@orange.fr>
2017-04-07 17:20:34 +0200

    creation de la fourmi situé aléatirement dans le tableau

--- app.js
+++ app.js
@@ -1,16 +1,30 @@
-$("#button").click(function(){
-	var ligne =$("#ligne").val()
-	var colonne = $("#colonne").val()
 
-	 for (var i = 0 ; i < ligne; i++) {
-	 	$("#tb").append('<tr></tr>');
-	 	for (var j = 0; j < colonne ; j++) {
-	 		$("#tb tr:last-child").append("<td data-coord=x"+i+"y"+j+"></td>");
+
+	$("#button").click(function(){
+		var ligne =$("#ligne").val()
+		var colonne = $("#colonne").val()
+
+		 for (var i = 0 ; i < ligne; i++) {
+		 	$("#tb").append('<tr></tr>');
+		 	for (var j = 0; j < colonne ; j++) {
+		 		$("#tb tr:last-child").append("<td id=x"+i+"y"+j+" data-coord=x"+i+"y"+j+"></td>");
+			}
 		}
-	console.log(ligne);
-	console.log(colonne);
-	}
+var ord = Math.round(Math.random()*ligne);
+var abs = Math.round(Math.random()*colonne);
+console.log(ord)
+$("#x"+ord+"y"+abs+"").append("#=#:");
 
-});
+console.log(ord)
+console.log(abs)
+	});
+
+
+
+
+var ligne =$("#ligne").val()
+var colonne = $("#colonne").val()
+
+
 
 
